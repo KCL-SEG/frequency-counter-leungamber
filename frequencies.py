e9a720b05e63992745820c7040d50b352b5afa1f
Amber <amberwyleung@gmail.com>
2022-12-04 13:41:15 +0000

    complete frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        str_item = str(item)
+        count = 0
+        for other in items:
+            if str(other) == str_item:
+                count += 1
+        if not frequencies:
+            frequencies[str_item] = count
+        elif frequencies and str_item not in frequencies:
+            frequencies[str_item] = count
     return frequencies
